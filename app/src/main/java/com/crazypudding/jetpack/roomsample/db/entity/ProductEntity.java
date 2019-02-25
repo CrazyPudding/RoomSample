a138e75325f488ddc1521ef61f883faff2ff8165
CrazyPudding <puddingleng@gmail.com>
2019-02-25 10:48:53 +0800

    设置索引

--- app/src/main/java/com/crazypudding/jetpack/roomsample/db/entity/ProductEntity.java
+++ app/src/main/java/com/crazypudding/jetpack/roomsample/db/entity/ProductEntity.java
@@ -4,6 +4,7 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
 import android.arch.persistence.room.TypeConverters;
 
@@ -14,7 +15,8 @@ import java.util.Date;
 @Entity(tableName = "products",
         foreignKeys = {@ForeignKey(entity = PlaceEntity.class,
                 parentColumns = "id",
-                childColumns = "place_id")})
+                childColumns = "place_id")},
+        indices = {@Index(value = "place_id")})
 @TypeConverters(DateConverter.class)
 public class ProductEntity {
     @PrimaryKey(autoGenerate = true)
